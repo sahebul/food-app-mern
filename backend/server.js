0bc151aaf740f645ad2b4d0e5a375e19211c93ad
Sahebul <sahebul99@gmail.com>
2022-08-30 15:57:20 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,19 @@ const app = express();
 dotenv.config();
 mongoConnect();
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("Api being called");
-})
+
+const __dirname1= path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname1,"/frontend/build")))
+    app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+    })
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 
 app.use(express.static('uploads'));
 app.use('/images', express.static('uploads'));
